d1759eaa25330733cb6bcc4734b701fdbceca721
Shivangi Srivastava <srivastavashivangi356@gmail.com>
2022-01-16 14:19:23 +0530

    Add files via upload

---
+++ Microsoft/SpiralTraverseMatrix.cpp
@@ -0,0 +1,27 @@
+void spiralPrint(int m, int n, int a[R][C])
+{
+    int i, k = 0, l = 0;
+
+    while (k < m && l < n) {
+        for (i = l; i < n; ++i) {
+            cout << a[k][i] << " ";
+        }
+        k++;
+        for (i = k; i < m; ++i) {
+            cout << a[i][n - 1] << " ";
+        }
+        n--;
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                cout << a[m - 1][i] << " ";
+            }
+            m--;
+        }
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                cout << a[i][l] << " ";
+            }
+            l++;
+        }
+    }
+}
